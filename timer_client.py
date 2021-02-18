16dbdc9a11de875e88494d5832b05c949687e957
DuncanRL <59705125+DuncanRL@users.noreply.github.com>
2021-02-18 12:05:04 -0400

    Working Timer

--- timer_client.py
+++ timer_client.py
@@ -4,6 +4,23 @@ import json
 import socket
 from threading import Thread
 import tkinter as tk
+import tkinter.font as tkFont
+from tkinter import messagebox
+from os.path import expanduser, isfile, join, abspath
+from os import mkdir, getcwd
+import webbrowser
+from sys import platform, maxsize
+
+version = "v1.0.0"
+
+
+def resource_path(relative_path):
+    try:
+        from sys import _MEIPASS
+        base_path = _MEIPASS
+    except Exception:
+        base_path = abspath(".")
+    return join(base_path, relative_path)
 
 
 class SyncedTime:
@@ -35,6 +52,7 @@ class TimerClient:
         self.syncedTime = SyncedTime()
         self.startTime = self.syncedTime.time()
         self.pauseTime = 0.0
+        self.failed = False
 
     def getTime(self):
         if self.status in ["disconnected", "connecting", "stopped"]:
@@ -52,12 +70,12 @@ class TimerClient:
 
     def connect(self, addr="127.0.0.1", port=25564):
         if self.socket != None:
-            self.socket.close()
-        self.socket = socket.socket()
+            self.disconnect()
 
+        self.socket = socket.socket()
         fails = 0
         for i in range(3):
-            print("Connection attempt "+str(i+1))
+            #print("Connection attempt "+str(i+1))
             try:
                 self.status = "connecting"
                 self.socket.connect((addr, port))
@@ -65,7 +83,8 @@ class TimerClient:
             except:
                 fails += 1
         if fails == 3:
-            print("Connection failed")
+            #print("Connection failed")
+            self.failed = True
         else:
             self.status = "stopped"
             self.recvLoopThread = Thread(target=self.recvLoop)
@@ -73,11 +92,18 @@ class TimerClient:
 
     def disconnectionEvent(self):
         self.status = "disconnected"
-        print("Disconnected")
+        #print("Disconnected")
         try:
             self.socket.close()
         except:
             pass
+    
+    def getFailed(self):
+        if self.failed:
+            self.failed = False
+            return True
+        else:
+            return False
 
     def startTimeEvent(self):
         pass  # Play sound?
@@ -91,13 +117,12 @@ class TimerClient:
 
             self.disconnectionEvent()
         else:
-            print("Already disconnected.")
+            pass#print("Already disconnected.")
 
     def recvLoop(self):
         while True:
             try:
                 msg = self.socket.recv(1024).decode()
-
                 if msg == "end":
                     self.disconnectionEvent()
                     break
@@ -119,11 +144,162 @@ class TimerClient:
                 break
 
 
-mtc = TimerClient()
+class TimerWindow(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
 
-mtc.connect()
+        self.parent = parent
+        self.connectMenu = None
+        self.parent.protocol("WM_DELETE_WINDOW", self.exit)
 
-while True:
-    time.sleep(0.1)
-    if mtc.isConnected():
-        print(mtc.getTime())
+        self.parent.bind('c', self.openConnectMenu)
+        self.parent.bind('d', self.disconnect)
+
+        self.timerClient = TimerClient()
+        self.font = tkFont.Font(self, ("Arial", 50))
+
+        self.defaultOptions = {"display": {
+            "name": "Arial", "size": 50, "color": "#ffffff", "bg": "#000000"}, "lastConnect": ""}
+        self.text = tk.Label(self, text=self.convertSeconds(0), font=self.font,width=50,anchor="w")
+        self.text.pack()
+
+        if "win" in platform:
+            self.optionsFolderPath = expanduser(
+                "~/AppData/Roaming/.cooptimer/")
+            self.optionsPath = expanduser(
+                "~/AppData/Roaming/.cooptimer/options.json")
+        else:
+            self.optionsFolderPath = getcwd()
+            self.optionsPath = join(getcwd(), "options.json")
+
+        self.optionsJson = None
+        self.loadSettings()
+
+        self.after(0, self.loop)
+    
+    def disconnect(self,x=0):
+        self.timerClient.disconnect()
+
+    def openConnectMenu(self,x=0):
+        if self.connectMenu == None:
+            self.connectMenu = ConnectMenu(self)
+    
+    def exit(self):
+        self.timerClient.disconnect()
+        self.destroy()
+        self.parent.destroy()
+
+    def loop(self):
+        self.after(int(1000/65), self.loop)
+
+        if self.timerClient.getFailed():
+            messagebox.showerror(message="Connection Failed")
+            self.openConnectMenu()
+
+        if self.timerClient.isConnected():
+            self.font.config(size=self.optionsJson["display"]["size"])
+            self.text.config(text=self.convertSeconds(
+                self.timerClient.getTime()))
+        else:
+            self.font.config(size=int(self.optionsJson["display"]["size"]/2.5))
+            self.text.config(text="Press 'c' to connect to a server.")
+
+    def loadSettings(self):
+        if not isfile(self.optionsPath):
+            try:
+                mkdir(self.optionsFolderPath)
+            except:
+                pass
+            with open(self.optionsPath, "w+") as optionsFile:
+
+                self.optionsJson = self.defaultOptions
+                json.dump(self.defaultOptions, optionsFile, indent=4)
+                optionsFile.close()
+        else:
+            with open(self.optionsPath, "r") as optionsFile:
+                self.optionsJson = json.load(optionsFile)
+                optionsFile.close()
+        self.validateOptions()
+        displayJson = self.optionsJson["display"]
+        self.parent.config(background=displayJson["bg"])
+        self.config(background=displayJson["bg"])
+        self.text.config(background=displayJson["bg"], fg=displayJson["color"])
+        self.font.config(family=displayJson["name"])
+
+    def validateOptions(self):
+        for i in self.defaultOptions:
+            try:
+                self.optionsJson[i]
+            except:
+                self.optionsJson[i] = self.defaultOptions[i]
+
+    @staticmethod
+    def convertSeconds(x):
+        if x < 0:
+            x = 0
+
+        Seconds = "%.3f" % (x-(int(x)-(int(x) % 60)))
+        if x % 60 < 10:
+            Seconds = "0" + Seconds
+        Minutes = str(int(x/(60)) % 60)
+        Hours = str(int(x/(60*60)))
+
+        if len(Minutes) < 2 and Hours != "0":
+            Minutes = "0" + Minutes
+
+        return ((Hours+":") if Hours != "0" else "")+Minutes+":"+Seconds
+
+
+class ConnectMenu(tk.Toplevel):
+    def __init__(self, parent):
+        tk.Toplevel.__init__(self, parent)
+        self.parent = parent
+        self.protocol("WM_DELETE_WINDOW", self.exit)
+        self.inputbox = tk.Entry(self,width=20)
+        self.inputbox.grid(column=0,row=0,padx=2,pady=2)
+        self.inputbox.insert(0,self.parent.optionsJson["lastConnect"])
+        self.button = tk.Button(self,text="Connect",command=self.connect,width=8)
+        self.button.grid(column=1,row=0,padx=2,pady=2)
+
+    def exit(self):
+        self.parent.connectMenu = None
+        self.destroy()
+    
+    def connect(self):
+        output = self.inputbox.get().replace(" ","").rstrip()
+        self.parent.optionsJson["lastConnect"] = output
+
+        args = output.split(":")
+
+        if len(args) == 0:
+            pass
+        else:
+            valid = False
+            if len(args) == 1:
+                args.append(25564)
+                valid = True
+                
+            else:
+                try:
+                    args[1] = int(args[1])
+                    valid = True
+                except:
+                    messagebox.showerror(message="Invalid Port!")
+                    
+            if valid:
+                self.parent.connectMenu = None
+                self.destroy()
+                self.parent.timerClient.connect()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Coop Timer Client "+version)
+    root.geometry("400x90")
+    try:
+        root.iconbitmap(resource_path("Icon.ico"))
+    except:
+        pass
+    tw = TimerWindow(root)
+    tw.grid(padx=2, pady=2, row=0, column=0, sticky="w")
+    root.mainloop()
